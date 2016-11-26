50c41085ce397b2326ac5e405323ec51c2104f1a
PeschanskyVlad <kpi.kp52.peschansky@gmail.com>
2016-11-26 11:17:55 +0200

    api

--- routes/users.js
+++ routes/users.js
@@ -15,4 +15,48 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.post('/', function(req, res) {
+  var user = new UserTemplate({
+    username : req.body.username,
+    email : req.body.email,
+    password : req.body.password,
+    image : req.body.image,
+    admin : false,
+  });
+
+  user.save((err) => {
+    if(!err){
+      res.send({Status : 'ok\n', user : user});
+    } else {
+      console.log(err);
+      if(err.name == 'ValidationError') {
+               res.statusCode = 400;
+               res.send({ error: 'Validation error' });
+           } else {
+               res.statusCode = 500;
+               res.send({ error: 'Server error' });
+           }
+    }
+  });
+});
+
+
+
+router.delete('/:id', (req, res) => {
+    UserTemplate.findById(req.params.id, (err, user) => {
+      if(!user){
+        res.statusCode = 404;
+        res.send({error : 'Not Found'});
+      }
+      user.remove((err) => {
+        if(!err){
+          res.send({Status : 'OK'});
+        } else {
+          res.statusCode = 500;
+          res.send({error : 'Server Error'});
+        }
+      });
+    });
+});
+
 module.exports = router;
